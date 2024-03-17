a762f02eefd266836b661863885bb6019ab2bf06
Andrzej Łątkowski <s26442@pjwstk.edu.pl>
2024-03-17 14:03:58 +0100

    added more variables to array, now it's tested and finished

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -35,7 +35,7 @@ internal abstract class Program
 {
     private static void Main()
     {
-        int[] numbers = [1, 2, 3, 4];
+        int[] numbers = [5, 1, 2, 13, 3, 4];
         double res = Lab1.Average(numbers);
         Console.WriteLine(res);
 
