eb99e447716c40f209c444912f9f76bcd4ef2a3b
Andrzej Łątkowski <s26442@pjwstk.edu.pl>
2024-03-17 13:41:26 +0100

    implemented static count average method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace ConsoleApp1;
 
-Console.WriteLine("MOD1");
-Console.WriteLine("MOD2");
-Console.WriteLine("MOD3");
+public static class Lab1
+{
+    public static double Average(int[] x)
+    {
+        double sum = 0;
+        foreach (var t in x)
+        {
+            sum += t;
+        }
+
+
+        double res = sum / x.Length;
+        return res;
+    }
+}
+
+internal abstract class Program
+{
+    private static void Main()
+    {
+        int[] numbers = [1, 2, 3, 4];
+        double res = Lab1.Average(numbers);
+        Console.WriteLine(res);
+    }
+}
